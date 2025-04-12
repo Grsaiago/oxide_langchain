38b43c8d60bfbf747530d942bf516cd3a2c8bfb0
Gabriel Saiago <grsaiago@gmail.com>
2025-04-12 00:09:54 -0300

    feat: add new methods to (a)runnable traits

--- oxide-langchain-core/src/traits/arunnable.rs
+++ oxide-langchain-core/src/traits/arunnable.rs
@@ -1,10 +1,46 @@
 use async_trait::async_trait;
-use serde_json::Value;
+use serde_json::{Map, Number, Value};
 use std::error::Error;
 
 #[async_trait]
 pub trait ARunnable {
-    async fn ainvoke(&self, input: Value) -> Result<Value, Box<dyn Error>>;
+    async fn ainvoke(&mut self, input: Value) -> Result<Value, Box<dyn Error>> {
+        match input {
+            Value::Null => self.handle_null_input().await,
+            Value::Bool(input) => self.handle_bool_input(input).await,
+            Value::Number(input) => self.handle_number_input(input).await,
+            Value::String(input) => self.handle_string_input(input).await,
+            Value::Array(input) => self.handle_array_input(input).await,
+            Value::Object(input) => self.handle_object_input(input).await,
+        }
+    }
+
+    async fn handle_null_input(&mut self) -> Result<Value, Box<dyn Error>> {
+        unimplemented!()
+    }
+
+    async fn handle_bool_input(&mut self, input: bool) -> Result<Value, Box<dyn Error>> {
+        unimplemented!()
+    }
+
+    async fn handle_number_input(&mut self, input: Number) -> Result<Value, Box<dyn Error>> {
+        unimplemented!()
+    }
+
+    async fn handle_string_input(&mut self, input: String) -> Result<Value, Box<dyn Error>> {
+        unimplemented!()
+    }
+
+    async fn handle_array_input(&mut self, input: Vec<Value>) -> Result<Value, Box<dyn Error>> {
+        unimplemented!()
+    }
+
+    async fn handle_object_input(
+        &mut self,
+        input: Map<String, Value>,
+    ) -> Result<Value, Box<dyn Error>> {
+        unimplemented!()
+    }
 }
 
 #[async_trait]
@@ -12,9 +48,9 @@ impl<T> ARunnable for Vec<T>
 where
     T: ARunnable + Sync + Send,
 {
-    async fn ainvoke(&self, input: Value) -> Result<Value, Box<dyn Error>> {
+    async fn ainvoke(&mut self, input: Value) -> Result<Value, Box<dyn Error>> {
         let mut acc = input;
-        for item in self.iter() {
+        for item in self.iter_mut() {
             acc = item.ainvoke(acc).await?;
         }
         Ok(acc)
--- oxide-langchain-core/src/traits/runnable.rs
+++ oxide-langchain-core/src/traits/runnable.rs
@@ -1,8 +1,50 @@
-use serde_json::Value;
+use serde_json::{Map, Number, Value};
 use std::error::Error;
 
 pub trait Runnable {
-    fn invoke(&mut self, input: Value) -> Result<Value, Box<dyn Error>>;
+    fn invoke(&mut self, input: Value) -> Result<Value, Box<dyn Error>> {
+        match input {
+            Value::Null => self.handle_null_input(),
+            Value::Bool(input) => self.handle_bool_input(input),
+            Value::Number(input) => self.handle_number_input(input),
+            Value::String(input) => self.handle_string_input(input),
+            Value::Array(input) => self.handle_array_input(input),
+            Value::Object(input) => self.handle_object_input(input),
+        }
+    }
+
+    fn handle_null_input(&mut self) -> Result<Value, Box<dyn Error>> {
+        unimplemented!()
+    }
+
+    fn handle_bool_input(&mut self, input: bool) -> Result<Value, Box<dyn Error>> {
+        unimplemented!()
+    }
+
+    fn handle_number_input(&mut self, input: Number) -> Result<Value, Box<dyn Error>> {
+        unimplemented!()
+    }
+
+    fn handle_string_input(&mut self, input: String) -> Result<Value, Box<dyn Error>> {
+        unimplemented!()
+    }
+
+    fn handle_array_input(&mut self, input: Vec<Value>) -> Result<Value, Box<dyn Error>> {
+        unimplemented!()
+    }
+
+    fn handle_object_input(&mut self, input: Map<String, Value>) -> Result<Value, Box<dyn Error>> {
+        unimplemented!()
+    }
+}
+
+impl<R> Runnable for Box<R>
+where
+    R: Runnable + ?Sized,
+{
+    fn invoke(&mut self, input: Value) -> Result<Value, Box<dyn Error>> {
+        self.invoke(input)
+    }
 }
 
 impl<T> Runnable for Vec<T>
@@ -17,9 +59,9 @@ where
     }
 }
 
-impl<T> Runnable for T
+impl<F> Runnable for F
 where
-    T: FnMut(Value) -> Result<Value, Box<dyn Error>>,
+    F: FnMut(Value) -> Result<Value, Box<dyn Error>>,
 {
     fn invoke(&mut self, input: Value) -> Result<Value, Box<dyn Error>> {
         self(input)
