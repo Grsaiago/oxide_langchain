f70e630ac5e9faca226df2d86f731ccbc9b3ef6a
Gabriel Saiago <grsaiago@gmail.com>
2025-05-03 20:37:55 -0300

    fix(traits): remove inputs traits

--- oxide-langchain-core/src/traits/mod.rs
+++ oxide-langchain-core/src/traits/mod.rs
@@ -4,11 +4,5 @@ pub use arunnable::ARunnable;
 mod runnable;
 pub use runnable::Runnable;
 
-mod inputs;
-pub use inputs::{
-    HandlesArrayAsInput, HandlesBoolAsInput, HandlesNumberAsInput, HandlesObjectAsInput,
-    HandlesStringAsInput,
-};
-
 mod document;
 pub use document::Document;
